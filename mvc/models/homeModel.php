89404762019e384dbd9053cdd63d1a02bfb2a284
thuynnt262 <84000068+thuynnt262@users.noreply.github.com>
2022-07-04 22:14:33 +0700

    Cap nhat mot so thu

--- mvc/models/homeModel.php
+++ mvc/models/homeModel.php
@@ -6,17 +6,4 @@
          {
              echo 'Hello World!';
          }
-
-         public function getData()
-         {
-            $query = "SELECT * FROM members";
-            $raw = $this->dbConnect->query($query);
-
-            $result = [];
-            while ($row = $raw->fetch_assoc()) {
-                array_push($result, $row);
-            }
-
-            return $result;
-         }
      }
--- mvc/views/homeView.php
+++ mvc/views/homeView.php
@@ -22,32 +22,144 @@
 
      <?php require_once 'mvc/views/blocks/header.php'; ?>
 
+     <div class="banner-main_home"> </div>
+
      <main>
-          <div class="banner-main_home">
-               <div style="padding: 200px 0">
-                    <?php
-                         $testData = isset($data['testData']) ? $data['testData'] : [];
-                    ?>
-                    <table class="table mx-auto is-bordered is-striped is-narrow is-hoverable">
-                         <tr>
-                              <th>ID</th>
-                              <th>Họ và Tên</th>
-                              <th>Vai trò</th>
-                         </tr>
-                         <?php
-                              foreach ($testData as $dataRow) {
-                                  echo '
-                                        <tr>
-                                             <td>'.$dataRow['id'].'</td>
-                                             <td>'.$dataRow['full_name'].'</td>
-                                             <td>'.$dataRow['role'].'</td>
-                                        </tr> 
-                                   ';
-                              }
-                         ?>
-                    </table>
+
+          <section class="home__title-bar">
+               <div class="title-bar__list">
+                    <div class="title-bar__item">
+                         <div class="title-bar__img">
+                              <img src="https://res.cloudinary.com/foodstocloud/image/upload/v1656752379/icon_images/tt-home-1_rlyid0.png" alt="">
+                         </div>
+                         <div class="in-item">
+                              <h5>Nông sản</h5>
+                              Nông sản tươi - sạch
+                         </div>
+                    </div>
+                    <div class="title-bar__item">
+                         <div class="title-bar__img">
+                              <img src="https://res.cloudinary.com/foodstocloud/image/upload/v1656752379/icon_images/tt-home-2_epec9h.png" alt="">
+                         </div>
+                         <div class="in-item">
+                              <h5>Salads</h5>
+                              Salads giàu dinh dưỡng
+                         </div>
+                    </div>
+                    <div class="title-bar__item">
+                         <div class="title-bar__img">
+                              <img src="https://res.cloudinary.com/foodstocloud/image/upload/v1656752380/icon_images/tt-home-3_rqihgk.png" alt="">
+                         </div>
+                         <div class="in-item">
+                              <h5>Giỏ hàng</h5>
+                              Giỏ hàng tiện lợi
+                         </div>
+                    </div>
+                    <div class="title-bar__item">
+                         <div class="title-bar__img">
+                              <img src="https://res.cloudinary.com/foodstocloud/image/upload/v1656752380/icon_images/tt-home-4_ano0b4.png" alt="">
+                         </div>
+                         <div class="in-item">
+                              <h5>Hỗ trợ</h5>
+                              Hỗ trợ nhanh
+                         </div>
+                    </div>
                </div>
-          </div>
+          </section>
+
+          <section class="home__services-con">
+               <div class="services-list">
+                    <div class="services-item ">
+                         <div class="service-img ser-img1"></div>
+                         <div class="ser-con">
+                              <span>Sản phẩm hữu cơ</span>
+                              <h4>Rau củ</h4>
+                              <a href=""> Cửa hàng </a>
+                         </div>
+                    </div>
+                    <div class="services-item ">
+                         <div class="service-img ser-img2"></div>
+                         <div class="ser-con">
+                              <span>Giảm giá 30%</span>
+                              <h4>Thực phẩm sạch</h4>
+                              <a href=""> Cửa hàng </a>
+                         </div>
+                    </div>
+                    <div class="services-item ">
+                         <div class="service-img ser-img3"></div>
+                         <div class="ser-con">
+                              <span>Không chứa Gluten</span>
+                              <h4>Super Salads</h4>
+                              <a href=""> Cửa hàng </a>
+                         </div>
+                    </div>
+               </div>
+          </section>
+
+          <section class="home__title">
+               <div class="title-hident">
+                    Sản phẩm
+               </div>
+               <div class="title-main">
+                    <span>Sản phẩm hữu cơ</span>
+                    <h2>Món ăn kèm hữu cơ</h2>
+                    <div class="title-divider"></div>
+               </div>
+          </section>
+
+          <section class="home_product-bar">
+               <div class="home_product-filters">
+                    <button data-filter = ".breakfast-cereals" class="show active">Ngũ cốc ăn sáng</button>
+                    <button data-filter = ".fruit-yogurt">Sữa chua trái cây</button>
+                    <button data-filter = ".salads">Salads</button>
+               </div>
+               <div class="home_product">
+                    <div class="h-product-list .breakfast-cereals">
+                         <div class="h-product-main">
+                              <div class="h-product-item">
+                                   <a href="">
+                                        <div class="h-product-img">
+                                        <img src="https://res.cloudinary.com/foodstocloud/image/upload/v1655112834/product_images/d2qx4eynlriebzyhonkq.jpg" alt="">
+                                        </div>
+                                        <div class="h-product_content">
+                                             <div class="h-product__category">
+                                                  <span class="h-category">Ngũ cốc</span>
+                                                  ,
+                                                  <span class="h-category">Đồ ăn nhẹ</span>
+                                             </div>
+                                             <div class="h-product__title-price">
+                                                  <a href="">Khoai tây chiên</a>
+                                                  <div class="h-product__price">
+                                                       <div class="h-price">
+                                                       <span>10.000</span> VNĐ
+                                                       </div>
+                                                       <ul class="h-ratings">
+                                                            <li>
+                                                                 <i class="far fa-star"></i>
+                                                            </li>
+                                                            <li>
+                                                                 <i class="far fa-star"></i>
+                                                            </li>
+                                                            <li>
+                                                                 <i class="far fa-star"></i>
+                                                            </li>
+                                                            <li>
+                                                                 <i class="far fa-star"></i>
+                                                            </li>
+                                                            <li>
+                                                                 <i class="far fa-star"></i>
+                                                            </li>
+                                                       </ul>
+                                                  </div>
+                                             </div>
+                                        </div>
+                                   </a>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </section>
+
      </main>
 
      <?php require_once 'mvc/views/blocks/footer.php'; ?>
