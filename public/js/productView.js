dede6272cc988188f4dcd84e707ce82413a03caa
HoMinhQuang <45233479+drakendark999@users.noreply.github.com>
2022-07-15 01:06:19 +0700

    feat(foodsto) : fix productView add scrollresize + fix categoryView add animation text

---
+++ public/js/productView.js
@@ -0,0 +1,31 @@
+const rangeInput = document.querySelectorAll(".range-input"),
+    priceInput =document.querySelectorAll(".price-label span")
+    progress = document.querySelector(".ui-slider-range");
+
+let priceGap = 10;
+
+if (!progress.style.width) {
+    progress.style.width = "100%";
+}
+rangeInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        progress.style.width = "";
+
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            } else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            let percent = (maxVal / rangeInput[1].max) * 100;
+            priceInput[0].innerHTML ="$"+ minVal;
+            priceInput[1].innerHTML ="$"+ maxVal;
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
