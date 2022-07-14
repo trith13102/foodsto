fe76f15211a4fb530aa240ceee7704c5547dcbce
thuynnt262 <84000068+thuynnt262@users.noreply.github.com>
2022-07-14 13:08:36 +0700

    Cap nhat home

--- public/js/homeView.js
+++ public/js/homeView.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
 
 
 
-
 $(document).ready(function(){
     $('.tabs__content').hide();
     $('.tabs__content:first-child').fadeIn();
@@ -30,13 +29,30 @@ $(document).ready(function(){
 });
 
 
-//Swiper
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-});
+window.onload=function(){
+    const btn_open = document.getElementById("btn-open");
+    const btn_close = document.getElementById("btn-close");
+    const modal_container = document.getElementById("modal-container");
+    const modal = document.getElementById("modal");
+
+    btn_open.addEventListener('click', () =>{
+        modal_container.classList.remove('hidden');
+    });
+    btn_close.addEventListener('click', () => {
+        modal_container.classList.add('hidden');
+    });
+    modal_container.addEventListener('click', (e) =>{
+        if (!modal.contains(e.target)) {
+            btn_close.click();
+        }
+    })
+
+    var btnPlus = document.getElementsByClassName('button-plus')[0];
+    var btnSub = document.getElementsByClassName('button-subtract')[0];
+    var btnValue = btnPlus.previousElementSibling;
+
+    btnPlus.onclick = () => btnValue.value++;
+    btnSub.onclick = () => (btnValue.value >= 2) ? (btnValue.value--) : "";
+}
+
+
