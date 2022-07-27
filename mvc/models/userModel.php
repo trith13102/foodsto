a34c0dde7995320b1c4642e3b686a0cd4a5fd42b
nlpn1408 <nlpn1481999@gmail.com>
2022-07-27 13:53:38 +0700

    login edit

--- mvc/models/userModel.php
+++ mvc/models/userModel.php
@@ -30,21 +30,32 @@ class UserModel extends Connect
       $row = mysqli_query($this->dbConnect, $qr);
       $count = mysqli_num_rows($row);
       $row_data =  mysqli_fetch_array($row);
+
       if ($count > 0) {
          if (password_verify($password, $row_data['password'])) {
             $result = true;
-            
-            if (!isset($_SESSION['is_logged_in'])) {
-               $_SESSION['is_logged_in'] = 1;
-            }
-            if (!isset($_SESSION['email'])) {
-               $_SESSION['email'] = $email;
-            }
-            if (!isset($_SESSION['password'])) {
-               $_SESSION['password'] = $password;
-            }
+            $this->UserLogin($row_data);
          }
+         return json_encode($result);
       }
-      return json_encode($result);
+   }
+
+   public function UserLogin($user){
+      if (!isset($_SESSION['is_logged_in'])) {
+         $_SESSION['is_logged_in'] = 1;
+      }
+      if (!isset($_SESSION['email'])) {
+         $_SESSION['email'] = $user['email'];
+      }
+      if (!isset($_SESSION['password'])) {
+         $_SESSION['password'] = $user['password'];
+      }
+
+      if (!isset($_SESSION['id'])) {
+         $_SESSION['id'] =  $user['id'];
+      }
+      if (!isset($_SESSION['fullname'])) {
+         $_SESSION['fullname'] =  $user['fullname'];
+      }
    }
 }
