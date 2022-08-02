7638c850e2aa5566677ed292d8164da4f88ef3e1
HoMinhQuang <45233479+drakendark999@users.noreply.github.com>
2022-08-02 21:52:57 +0700

    fix page product

--- mvc/models/productModel.php
+++ mvc/models/productModel.php
@@ -21,8 +21,20 @@ class productModel extends Connect
           $page = $this->getPage();
           $from = ($page - 1) * $this->showProduct;
 
-          if (isset($_GET['orderby']) && !$_GET['orderby'] == '') {
-               $orderby = $_GET['orderby'] ;
+          if (isset($_GET['min']) && !$_GET['min'] == '') {
+               $min = $_GET['min'] ?? 1000;
+               $max = $_GET['max'] ?? 650000;
+               $sql = "SELECT *,Sort.name AS productName ,
+                         Sort.thumbnail as productThumbnail,
+                         Sort.id as productId
+                         FROM (SELECT * FROM products 
+                         WHERE price BETWEEN $min and $max) AS Sort
+                         INNER JOIN categories 
+                         ON Sort.category_id = categories.id
+                         LIMIT $from,$this->showProduct";
+          } else {
+
+               $orderby = $_GET['orderby'] ?? null;
                $sort = match ($orderby) {
                     'price-desc' => $sql =  "SELECT *,Sort.name AS productName ,
                                              Sort.thumbnail as productThumbnail,
@@ -50,17 +62,6 @@ class productModel extends Connect
                                         ON products.category_id = categories.id
                                         LIMIT $from,$this->showProduct "
                };
-          } elseif(isset($_GET['min'])&&!$_GET['min']==''){
-               $min=$_GET['min']?? 1000;
-               $max=$_GET['max']??650000;
-               $sql = "SELECT *,Sort.name AS productName ,
-                         Sort.thumbnail as productThumbnail,
-                         Sort.id as productId
-                         FROM (SELECT * FROM products 
-                         WHERE price BETWEEN $min and $max) AS Sort
-                         INNER JOIN categories 
-                         ON Sort.category_id = categories.id
-                         LIMIT $from,$this->showProduct";
           }
 
 
@@ -87,9 +88,9 @@ class productModel extends Connect
           $total_products = 0;
           if (isset($_GET['keyword']) && !$_GET['keyword'] == '') {
                $total_products = mysqli_num_rows($this->search());
-          } elseif (isset($_GET['min'])&&!$_GET['min']=='') {
-               $min=$_GET['min']?? 1000;
-               $max=$_GET['max']??650000;
+          } elseif (isset($_GET['min']) && !$_GET['min'] == '') {
+               $min = $_GET['min'] ?? 1000;
+               $max = $_GET['max'] ?? 650000;
                $sql  = "SELECT * FROM products 
                     WHERE price BETWEEN $min and $max";
                $query = mysqli_query($this->dbConnect, $sql);
@@ -100,11 +101,12 @@ class productModel extends Connect
                INNER JOIN products 
                ON products.category_id = categories.id ";
                $query = mysqli_query($this->dbConnect, $sql);
-               $total_products = mysqli_num_rows($query);       
+               $total_products = mysqli_num_rows($query);
           }
           return $total_products;
      }
-     public function pagesProduct() {
+     public function pagesProduct()
+     {
           $show_pages = ceil($this->totalProduct() / $this->showProduct);
           return $show_pages;
      }
